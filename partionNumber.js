3d572672afd44d78e9c9762df2658b48d83e4f47
chaospace <ichaospace@gmail.com>
2021-10-18 09:17:54 +0900

    [알고리즘] 수 분할 성질 정리

--- partionNumber.js
+++ partionNumber.js
@@ -13,22 +13,24 @@
  *  공식 : p(n, k) = p(n-k,1)+p(n-k,2) + .. + p(n-k, k);
  * 여기서 경우의 수를 탐색하며 내용을 보는 거는 실제로 만들어 봐야 한다.
  */
-
-function partitonNumber(n, m) {
+function partitonNumber(n, m, depth, arr = []) {
+  // 이게 곧 0 처리
   if (n < m) {
     m = n;
   }
-
+  let count = 0;
   if (n == 0) {
+    console.log("arr", arr);
     return 1;
   }
-
-  let count = 0;
   for (let i = 1; i <= m; i++) {
-    count += partitonNumber(n - i, i);
+    arr[depth] = i; // 뎁스에 해당하는 값을 배열에 기억
+    // 재귀 발생 시 마다 뎁스값을 증가시켜 전달
+    count += partitonNumber(n - i, i, depth + 1, arr);
+    // 해당 뎁스 완료 후 값을 0으로 초기화
+    // 고정 값 이후는 모두 1로 초기화(이게 없을 경우)
+    arr[depth] = 0;
   }
-
   return count;
 }
-
 console.log(partitonNumber(5, 5, 0));
