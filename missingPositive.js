6e9386e887c949bf71dead8e1360fe16c10c8114
chaospace <ichaospace@gmail.com>
2022-11-24 10:48:57 +0900

    [리팩토링] 빠진 정수 찾기 코드 개선

--- missingPositive.js
+++ missingPositive.js
@@ -33,37 +33,72 @@ function solution(nums) {
   //console.log("a", nums[idx], "idx", idx);
 }
 
-console.log(solution([1, 2, 0]));
-console.log(solution([3, 4, -1, 1]));
-console.log(solution([7, 8, 9, 11, 12]));
+// console.log(solution([1, 2, 0]));
+// console.log(solution([3, 4, -1, 1]));
+// console.log(solution([7, 8, 9, 11, 12]));
 
+function solutionB(nums) {
+  nums.sort((a, b) => a - b);
+  let idx = 0;
+  // 음수 제거
+  while (nums[idx] <= 0) {
+    idx++;
+  }
+
+  const stack = [];
+  for (let i = idx; i < nums.length; i++) {
+    const prev = stack[stack.length - 1] || 0;
+    const current = nums[i];
+    const diff = current - prev;
+    if (diff === 0) continue;
+    if (diff > 1) {
+      break;
+    }
+    stack.push(current);
+  }
+  return stack.length && stack[0] === 1 ? stack.length + 1 : 1;
+}
+console.log(solutionB([1, 2, 0]));
+console.log(solutionB([3, 4, -1, 1]));
+console.log(solutionB([7, 8, 9, 11, 12]));
+
+/**
+ * 배열 변경 방식이 특이함. 참고..
+ * @param {*} nums
+ * @returns
+ */
 var firstMissingPositive = function (nums) {
-  //   let i = 0;
-  //   while (i < nums.length) {
-  //     if (
-  //       nums[i] > 0 &&
-  //       nums[i] <= nums.length &&
-  //       nums[nums[i] - 1] !== nums[i]
-  //     ) {
-  //       [nums[nums[i] - 1], nums[i]] = [nums[i], nums[nums[i] - 1]];
-  //     } else {
-  //       i++;
-  //     }
-  //   }
-  // for (i = 0; i < nums.length; i++) {
-  //     if (nums[i] !== i + 1) return i + 1;
-  // }
-  // return i + 1;
+  let i = 0;
+  while (i < nums.length) {
+    if (
+      nums[i] > 0 &&
+      nums[i] <= nums.length &&
+      nums[nums[i] - 1] !== nums[i]
+    ) {
+      [nums[nums[i] - 1], nums[i]] = [nums[i], nums[nums[i] - 1]];
+    } else {
+      i++;
+    }
+  }
+  for (i = 0; i < nums.length; i++) {
+    if (nums[i] !== i + 1) return i + 1;
+  }
+  return i + 1;
 };
 
-firstMissingPositive([1, 2, 0]);
-firstMissingPositive([3, 4, -1, 1]);
+//firstMissingPositive([1, 2, 0]);
+//firstMissingPositive([3, 4, -1, 1]);
 
-const arr = [1, 2, 3, 4];
-console.log("before-arr", arr);
-[arr[2], arr[1]] = [arr[1], arr[2]];
-console.log("after-arr", arr);
+// const arr = [1, 2, 3, 4];
+// console.log("before-arr", arr);
+// [arr[2], arr[1]] = [arr[1], arr[2]];
+// console.log("after-arr", arr);
 
+/**
+ * 배열 정렬 후 empty인 index를 찾아서 반환
+ * @param {*} nums
+ * @returns
+ */
 function solutionSortArray(nums) {
   var numsLen = nums.length,
     index,
@@ -75,7 +110,7 @@ function solutionSortArray(nums) {
       sortedNums[nums[index]] = nums[index];
     }
   }
-  console.log("nums", nums, "sortedNums", sortedNums);
+
   for (
     index = 0, sortedNumsLen = sortedNums.length;
     index < sortedNumsLen;
@@ -88,3 +123,4 @@ function solutionSortArray(nums) {
   console.log("index", index);
   return index;
 }
+solutionSortArray([3, 4, -1, 1]);
