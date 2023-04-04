a1c2745fe4fd07780b778772a31c19b438c2a178
chaospace <ichaospace@gmail.com>
2023-04-04 13:06:50 +0900

    [알고리즘] 머지정렬 코드 개선

--- sort/mergeSort.js
+++ sort/mergeSort.js
@@ -50,6 +50,7 @@ function mergeSort(array, start, middle, end) {
   let max;
   let left = [];
   let right = [];
+
   // 좌측클론
   for (i = 0, cindex = start, max = middle; cindex <= max; i++, cindex++) {
     left[i] = array[cindex];
@@ -92,6 +93,79 @@ function recursive(array, p, r) {
   }
 }
 //, 9, 11, 6, 2
-const arr = [14, 7, 3, 12, -1, 0, 3, 9, -2];
-recursive(arr, 0, arr.length - 1);
-console.log("array", arr);
+//const arr = [14, 7, 3, 12, -1, 0, 3, 9, -2];
+const arr = [5, 1, 4, 3, 2];
+//console.log("aa", recursive(arr, 0, arr.length - 1));
+//console.log("array", arr);
+
+function mergeSort2(arr, start, end) {
+  if (start === end) return 0;
+  const mid = Math.floor((start + end) / 2);
+  // 정렬하며 이동한 횟수 카운트
+  let ret = mergeSort2(arr, start, mid) + mergeSort2(arr, mid + 1, end);
+
+  let temp = [];
+  let leftIndex = start,
+    rightIndex = mid + 1;
+
+  // // 좌측배열 구성
+  // let tempL = [];
+  // for (; leftIndex <= mid; ) {
+  //   tempL.push(arr[leftIndex++]);
+  // }
+
+  // //우측 배열 구성
+  // let tempR = [];
+  // for (; rightIndex <= end; ) {
+  //   tempR.push(arr[rightIndex++]);
+  // }
+
+  // //분리된 배열 하나로 정렬하며 머지
+  // for (
+  //   leftIndex = 0, rightIndex = 0;
+  //   leftIndex < tempL.length && rightIndex < tempR.length;
+
+  // ) {
+  //   //우측이 더 클 경우
+  //   if (tempL[leftIndex] < tempR[rightIndex]) {
+  //     temp.push(tempL[leftIndex++]);
+  //   } else {
+  //     ret += tempL.length - leftIndex;
+  //     temp.push(tempR[rightIndex++]);
+  //   }
+  // }
+
+  // // 나머지 temp에 추가
+  // for (; leftIndex < tempL.length; ) {
+  //   temp.push(tempL[leftIndex++]);
+  // }
+  // for (; rightIndex < tempR.length; ) {
+  //   temp.push(tempR[rightIndex++]);
+  // }
+
+  /**
+   * 좌우 배열을 나누고 머지하는 코드 개선
+   * 하나의 반복에서 처리할 수 있을 듯.
+   */
+
+  while (leftIndex <= mid || rightIndex <= end) {
+    // 우측값이 더 크거나 좌측값만 유효할 경우
+    if (
+      leftIndex <= mid &&
+      (rightIndex > end || arr[leftIndex] < arr[rightIndex])
+    ) {
+      temp.push(arr[leftIndex++]);
+    } else {
+      ret += mid - leftIndex + 1;
+      temp.push(arr[rightIndex++]);
+    }
+  }
+
+  for (let i = 0; i < temp.length; i++) {
+    arr[start + i] = temp[i];
+  }
+  return ret;
+}
+
+console.log("end", mergeSort2(arr, 0, arr.length - 1));
+console.log("arr", arr);
