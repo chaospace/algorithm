55c3b62da60dc70968ef32c9a290f9663365bc89
chaospace <ichaospace@gmail.com>
2024-04-14 14:44:27 +0900

    [DP-투포인터] 담을 수 있는 물의 양 구하기

--- trappingRainWater.js
+++ trappingRainWater.js
@@ -15,66 +15,56 @@ Input: height = [4,2,0,3,2,5]
 Output: 9
 
 골짜기를 찾는 법
- - 좌우에 높이를 알 수 있는 index부터 시작 ex 1
- - 가장 낮은 곳에서 좌우에 값이 높다면 주변 인덱스를 넓혀가기 시작.
- */
+ - 좌우 벽 높이보다 작은 곳이 발견되면 낮은 벽의 높이 만큼 물을 받을 수 있다.
+ - leftWall = [0, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3] 
+ - h        = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+ - rightWall= [3, 3, 3, 3, 3, 3, 3, 3, 2 ,2, 2 ,1]
+ - minWall  = [0, 1, 1, 2, 2, 2, 2, 3, 2, 2, 2, 1]
+ - water    = [0, 0, 1, 0, 1, 2, 1, 0, 0, 1, 0, 0]
 
-function solution(height) {
-  const len = height.length;
-  if (len < 3) {
-    return 0;
-  }
+ - 위 방법을 하나로 합치면 결국 two포인터를 이용한 비교처리 진행.
+   left  좌측 벽중 가장 높은 값 참조.
+   right 우측 벽중 가장 높은 값 참조.
 
-  let sum = 0;
-  //let stack = [];
-  for (let i = 1; i < len - 1; i++) {
-    const value = height[i];
-    let l = i - 1;
-    let r = i + 1;
-    if (height[l] > value && height[r] > value) {
-      // gap이 형성되는 포인트 찾기
-      while (height[l - 1] > height[l]) {
-        l--;
-      }
-      while (height[r + 1] > height[r]) {
-        r++;
-      }
-      console.log("l", height[l], "r", height[r], "current", height[i]);
-      // 웅덩이 값 추출
-    }
-  }
-  console.log("======================");
-}
-//solution([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]);
-//solution([4, 2, 0, 3, 2, 5]);
+   let current = 0;
+   let lRef = 0;          // 좌측인덱스
+   let rRef = h.length-1; // 우측인덱스
+   let left = h[lRef];    // 좌측벽 초기화
+   let right =h[rRef];    // 우측벽 초기화
 
-/**
- * 웅덩이가 있는 영역을 채워가나며 값을 채움.
- * 루프를 돌며 높은 값을 만나면 자신보다 큰 값이 나오기 전까지
- * 부족한 부분을 채우면 진행.
- *
- * 높은 값을 만나면 값을 변경
- * 변경된 기준을 가지고 다시 반복.
- *  - 위 방식에 문제점은?
- *   - 현재 값보다 큰 값이 있을 경우만 유효.
- *   - 현재가 가장 높은 값이면 공통 높이로 변경.
- */
+   if(left <= right) {
+    left  = Math.max(h[++lRef], left);
+   } else {
+    right = Math.max(h[--rRef], right);
+   }
+   current = Maht.min(left, right);
+   water += (current - h[index]);
+*/
 
-function fillDownArea(height) {
-  // 시작 높이 설정
-  let current = height[0];
-  const diffs = [];
-  for (let i = 1; i < height.length; i++) {
-    if (current > height[i]) {
-      // 부족한 값을 채움.
-      diffs.push(current - height[i]);
+function solution(h) {
+  let current = 0;
+  let lRef = 0;
+  let rRef = h.length - 1;
+  let left = h[lRef]; // 0
+  let right = h[rRef]; // 1
+  // [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+  //두 개의 포인터가 만날 때 까지 벽 높이 비교를 통해 담을 수 있는 물에 양을 구함.
+  while (lRef < rRef) {
+    //각 벽의 높이는 항상 큰 것을 참조.
+    left = Math.max(left, h[lRef]);
+    right = Math.max(right, h[rRef]);
+    // 우측벽이 크거나 같을 경우
+    if (left <= right) {
+      // 이전 벽과 현재 벽의 높이 차이 구하고 이 값이 0보다 크다면 담을 수 있는 물의 양이 된다.
+      current += left - h[lRef]; // lRef, rRef포인터를 참조 포인터와 같이 사용.
+      lRef += 1;
+      // 좌측벽이 더 높을 경우 우측벽의 이전과 현재 높이 차를 구하면 된다.
     } else {
-      // 기본 높이 변경
-      current = Math.max(current, height[i]);
+      current += right - h[rRef];
+      rRef -= 1;
     }
   }
-  console.log("diff", diffs);
+  console.log("current", current);
 }
 
-fillDownArea([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]);
-fillDownArea([4, 2, 0, 3, 2, 5]);
+solution([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]);
